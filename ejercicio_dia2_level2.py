1234b59d0847d374fcaf03ea505278524267564c
UP240326 <up240326@alumnos.upa.edu.mx>
2025-06-05 12:24:35 -0600

    Inicio del control de versiones

---
+++ ejercicio_dia2_level2.py
@@ -0,0 +1,36 @@
+# Level 2 Number 4
+num_one= 5
+num_two= 4
+# Level 2 Number 5
+sum_of_numbers = num_one + num_two
+print ("sum of numbers is:", sum_of_numbers)
+# Level 2 Number 6
+substract_numbers = num_one - num_two
+print("Substract of numbers is:", substract_numbers)
+# Level 2 Number 7
+multiply_numbers= num_one*num_two
+print("Multiplication fo numbers is:", multiply_numbers)
+# Level 2 Number 8
+divide_numbers = num_one/num_two
+print("Division of numbers is:", divide_numbers)
+# Level 2 Number 9
+mod_numbers= num_one%num_two
+print("Modulus of numbers is: ", mod_numbers)
+# Level 2 Number 10
+power_numbers = num_one**num_two
+print("Power of numbers is:", power_numbers)
+# Level 2 Number 11
+floordivision_numbers = num_one//num_two
+print("Floor division of numbers is:", floordivision_numbers)
+#Level 2 Number 12.1
+radius_of_circle=30
+area_of_circle= 3.14 * (radius_of_circle**2)
+print("Area of circle is:", area_of_circle)
+# Level 2 Number 12.2
+circumference_of_circle = 2 * 3.14 * radius_of_circle
+print("Circumference of circle is:", circumference_of_circle)
+# Level 2 Number 12.3
+radius= float(input ("Put the radius of the circle to calculate the new area:"))
+area_new = (3.14* (radius**2))
+print("Area of your new circle is:", area_new)
+# Level 2 Number 13
---
+++ ejercicios_dia1.py
@@ -0,0 +1,19 @@
+print (3+4) #addition
+print (3-4) #subtraction
+print (3*4) #multiplication
+print (3/4) #division
+print (3**4) #exponentiation
+print (3//4) #floor division
+print (3%4) #modulud
+#Level 2
+print ("My name is Cristian") #Name
+print ("My family name is Flores") #Family Name
+print ("My country is Mexico") #Country
+#Level 3
+print(type([1, 2, 3]))           # List
+print(type({'name':'Asabeneh'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
+print(type(3 == 3))              # Bool
+print(type(3 >= 3))              # Bool
+#Nivel 4 en file "helloword.py"
